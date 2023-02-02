193d9fabdcd824bb3ce8ba41da0e96cfa207b61d
Wyllwyll <terry.boeuf@hotmail.fr>
2023-02-02 16:32:53 +0100

    fix:bug import

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -36,29 +36,6 @@ export class User extends BaseEntity {
     zipCode : string
 
     @Column()
-    mail: string
-
-
-    @Column()
-    password: string
-
-
-    @Column()
-    adress_line1: string
-
-
-    @Column()
-    adress_line2: string
-
-
-    @Column()
-    adress_line3: string
-
-
-    @Column()
-    zipCode: string
-
-    @Column()
     city: string
 
     @OneToMany(() => Order, (order) => order.user)
