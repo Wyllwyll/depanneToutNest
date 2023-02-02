8cdcd72319c4144a30d3f6e11380c0789f6f300c
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-02-02 11:44:11 +0100

    feat : Add User Entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity("users")
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id : number
+
+    @Column()
+    username : string
+
+    
+    @Column()
+    mail : string
+
+    
+    @Column()
+    password : string
+
+    
+    @Column()
+    adress_line1 : string
+
+    
+    @Column()
+    adress_line2 : string
+
+    
+    @Column()
+    adress_line3 : string
+
+    
+    @Column()
+    zipCode : string
+
+    @Column()
+    city : string
+
+    @OneToMany(() => Order, (order) => order.user)
+    
+    orders : Order[]
+
+    
+}
