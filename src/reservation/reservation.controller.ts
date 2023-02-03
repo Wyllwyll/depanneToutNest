3efb6dbf206cc9c6d57ef8f461876ce433498bdd
FAVRE Jessie (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC HOURCADE GLA) <8009383b@commun.ad.sncf.fr>
2023-02-03 11:45:16 +0100

    get orderId

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -1,42 +1,29 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
-import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { FindAllReservationDto } from './dto/findall-reservation.dto';
+import { FindOneReservationDto } from './dto/find-reservation.dto';
 
-@Controller('reservation')
+@Controller('reservations')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) { }
 
 
-  @Post("addreservation")
+  @Post("add")
   create(@Body() createReservationDto: CreateReservationDto) {
     return this.reservationService.create(createReservationDto);
   }
 
 
-  @Get("allreservationsforoneuser")
-  findAll() {
+  @Get("forUser")
+  findAll(@Body() findAllReservationDto : FindAllReservationDto) {
+    console.log(findAllReservationDto);
+    
     return this.reservationService.findAllReservation();
   }
-}
 
-  /* findOne(@Body() id: string, updateOrderDto: UpdateOrderDto) {
-    return this.reservationService.findReservation(updateOrderDto);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reservationService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
-    return this.reservationService.update(+id, updateReservationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reservationService.remove(+id);
+@Get("orderId")
+  findOne(@Body() findonereservationDto: FindOneReservationDto) {
+    return this.reservationService.findOneReservation(findonereservationDto);
   }
 }
- */
--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { async } from 'rxjs';
 import { Order } from 'src/order/entities/order.entity';
 import { User } from 'src/users/entities/user.entity';
 import { CreateReservationDto } from './dto/create-reservation.dto';
-import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { FindOneReservationDto } from './dto/find-reservation.dto';
 import { Reservation } from './entities/reservation.entity';
 
 @Injectable()
@@ -14,34 +13,94 @@ export class ReservationService {
       newReservation.user = await User.findOneBy({id: createReservationDto.userId})//createReservationDto.id;
       newReservation.order = await Order.findOneBy({id: createReservationDto.order})
       newReservation.numero = newReservation.id+10
-      await newReservation.save();
-      return await User, Order.findOneBy(createReservationDto.userId, createReservationDto.order );
+      return await newReservation.save();
+      
     }
-  }
 
-   findAllReservation() {
+   async findAllReservation() {
     const reservation = await Reservation.find()
     return reservation;
   }
 
-
-/* function findAllReservation() {
-  throw new Error('Function not implemented.');
-}
-  /* findOne(id: number) {
-    return `This action returns a #${id} reservation`;
-  }
-
-  update(id: number, updateReservationDto: UpdateReservationDto) {
-    return `This action updates a #${id} reservation`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} reservation`;
+  async findOneReservation(findOneReservationDto: FindOneReservationDto){
+    return await Reservation.findOneBy({order : {id : findOneReservationDto.orderId}})
   }
 }
-function findAllReservation() {
-  throw new Error('Function not implemented.');
-}
 
+/**
+ * find :
+ * Trouver un ensemble de lignes dans un tableau
+ * 
+ * Exemple : Tous les users :
+ * User.find()
+ * 
+ * Exemple : Un user avec son id  (return une liste de users):
+ * User.find(
+ *  {
+ *    where : {
+ *     id : userId
+ *    }
+ *  }
+ * )
+ * 
+ * OU : avec le .findBy :
+ * User.findBy({id: userId}) 
+ * 
+ * Exemple : Avec 2 conditions ou +
+ * User.find(
+ *  {
+ *    where : {
+ *      id : userId,
+ *      name : userName
+ *    }
+ *  }
+ * )
+ * 
+ * Exemple : Seulement le userId :
+ *  User.find(
+ *  {
+ *    select : {
+ *      id : true,
+ *      password : true,
+ *      email : false
+ *    },
+ *    where : {
+ *     id : userId
+ *    }
+ *  }
+ * )
+ * 
+ * 
+ * TABLES CROISEES :
+ * Table User { id , name , pass }
+ * Table Order { id , user , price }
+ * 
+ * Liaison : User.orders  <=> Order.user
+ * 
+ * 
+ * Exemple : Tous les user avec leur Orders :
+ * User.find(
+ *  {
+ *    relations : {
+ *      orders : true
+ *    }
+ *  }
+ * )
+ * 
+ * Exemple : Tous les user avec leur Orders id :
+ * User.find(
+ *  {
+ *    select :{
+ *      ID :
+ *      orders : {
+ *        id : true
+ *      }
+ *    },
+ *    relations : {
+ *      orders : true
+ *    }
+ *  }
+ * )
+ * 
+ * 
  */
