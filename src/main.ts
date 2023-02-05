564be19cb19deb19c5e989920c5c68cdd37ff9fe
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-02-05 20:02:07 +0100

    feat : ajout swager

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './errors/AllExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()) ;
-  app.useGlobalFilters(new HttpExceptionFilter()) ;
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('DepanneTout')
+    .setDescription('Echange de services entre voisins')
+    .setVersion('1.0')
+    .addTag('voisins')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
