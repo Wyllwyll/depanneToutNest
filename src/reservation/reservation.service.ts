287471ad52b3a6dd0a3dd5f6c654706b3f952143
Captainbigleu <jessie.favre@live.fr>
2023-02-05 19:11:17 +0100

    foruser

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -28,6 +28,9 @@ export class ReservationService {
    * La fonction doit retourner la liste des Resa correspondant au userId
    */
   async findUserReservation(userId : number){
+    const reservations = await Reservation.findBy({id: userId}) 
+
+    return await reservations
     
   }
 
